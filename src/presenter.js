4f9c11f512560b6ff84733b503d2cb5cc7bdce68
Valeria Rivero <valeria.rivero@ucb.edu.bo>
2024-02-08 09:40:17 -0400

    Se añadio la funcionalidad de Multiplicar

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,27 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const form2 = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#resultado2-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
-
+  
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+  
+  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
